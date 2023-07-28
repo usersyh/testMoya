07db043ee7fe13c54647822336029cef3bea4338
usersyh <syu4809@naver.com>
2023-07-28 17:42:16 +0900

    수정

--- Hello/src/main/HelloJava.java
+++ Hello/src/main/HelloJava.java
@@ -4,7 +4,7 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("깃허브 테스트 하는 중~~~~~~~");
+		System.out.println("깃허브 수정 하는 중~!!!");
 	}
 
 }
